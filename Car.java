4f19234619f2b1e38c0e044112afd7f8cc800a91
Emma <emben@chalmers.se>
2026-01-27 15:16:11 +0100

    version 1

---
+++ Car.java
@@ -0,0 +1,75 @@
+import java.awt.*;
+
+public class Car implements Movable{
+
+    private double x,y;
+    private  int direction;
+    protected int nrDoors; // Number of doors on the car
+    protected double enginePower; // Engine power of the car
+    protected double currentSpeed; // The current speed of the car
+    protected Color color; // Color of the car
+    public String modelName; // The car model name
+
+    public Car(double x, double y, int direction){
+        this.x = x;
+        this.y = y;
+        this.direction = direction;
+    }
+
+    public int getNrDoors(){
+
+        return nrDoors;
+    }
+    public double getEnginePower(){
+
+        return enginePower;
+    }
+
+    public double getCurrentSpeed(){
+
+        return currentSpeed;
+    }
+
+    public Color getColor(){
+
+        return color;
+    }
+
+    public void setColor(Color clr){
+
+        color = clr;
+    }
+
+    public void startEngine(){
+
+        currentSpeed = 0.1;
+    }
+
+    public void stopEngine(){
+
+        currentSpeed = 0;
+    }
+// direcation kan vara 0,1,2,3 där 0 och 2 är uppåt respektive neråt, 3 och 1 är vänster respektive höger
+
+    @Override
+    public void move() {
+        if (direction == 0){
+            y = y - currentSpeed;
+        } else if (direction == 1) {
+            x = x+ currentSpeed;
+        } else if (direction==2) {
+            y = y + currentSpeed;
+        }else if (direction == 3);
+            x = x-currentSpeed;
+    }
+
+    @Override
+    public void turnLeft() {
+        direction = (direction -1 + 4) % 4;
+    }
+
+    @Override
+    public void turnRight() {
+        direction = (direction + 1 + 4) % 4;
+    }
+}
--- Volvo240.java
+++ Volvo240.java
@@ -1,49 +1,19 @@
 import java.awt.*;
 
-public class Volvo240{
+public class Volvo240 extends Car {
 
     public final static double trimFactor = 1.25;
-    public int nrDoors; // Number of doors on the car
-    public double enginePower; // Engine power of the car
-    public double currentSpeed; // The current speed of the car
-    public Color color; // Color of the car
-    public String modelName; // The car model name
     
-    public Volvo240(){
+    public Volvo240(double x, double y, int direction){
+
+        super(x, y,direction);
         nrDoors = 4;
         color = Color.black;
         enginePower = 100;
         modelName = "Volvo240";
         stopEngine();
     }
-    
-    public int getNrDoors(){
-        return nrDoors;
-    }
-    public double getEnginePower(){
-        return enginePower;
-    }
 
-    public double getCurrentSpeed(){
-        return currentSpeed;
-    }
-
-    public Color getColor(){
-        return color;
-    }
-
-    public void setColor(Color clr){
-	    color = clr;
-    }
-
-    public void startEngine(){
-	    currentSpeed = 0.1;
-    }
-
-    public void stopEngine(){
-	    currentSpeed = 0;
-    }
-    
     public double speedFactor(){
         return enginePower * 0.01 * trimFactor;
     }
@@ -66,3 +36,4 @@ public class Volvo240{
         decrementSpeed(amount);
     }
 }
+
